5e734a47d8822ca3c72ae438dbb184fc111d1462
HemaBindu123 <Hemabindukatta@gmail.com>
2025-03-07 20:45:55 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("signup").style.display = "none";
+}
+
+function showSignup() {
+    document.getElementById("signup").style.display = "block";
+    document.getElementById("login").style.display = "none";
+}
